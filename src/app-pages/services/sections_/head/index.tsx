0f425bef4be3563d71d8002380ee75a68d09325d
hazratahmdzd <B062192021@asoiu.edu.az>
2025-05-07 19:57:16 +0400

    updated Accordion functionality

--- src/app-pages/services/sections_/head/index.tsx
+++ src/app-pages/services/sections_/head/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Head = () => {
   return (
-    <div className="container-center mt-6 mb-4 sm:mt-12 sm:mb-6 lg:mt-18 lg:mb-12">
+    <div className="container-center mt-6 mb-4 sm:mt-12 sm:mb-6 lg:mt-18 lg:mb-12 transition-all">
       <div className="flex items-end gap-5 justify-between pb-8 border-b border-brand-foreground">
         <div className="flex flex-col gap-4 sm:gap-6 lg:gap-8 w-full lg:max-w-[780px]">
           <Star />
--- src/components/accordion/service-accordion.tsx
+++ src/components/accordion/service-accordion.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionContent,
@@ -9,11 +10,15 @@ import Link from "next/link";
 import { SERVICES_DATA } from "@/config/constants";
 
 export const ServiceAccordion = () => {
+  const [openItem, setOpenItem] = useState<string | null>(null);
+
   return (
     <Accordion
       type="single"
       collapsible
       className="w-full lg:w-[35%] flex flex-col gap-3"
+      value={openItem as string | undefined}
+      onValueChange={(value) => setOpenItem(value)}
     >
       {SERVICES_DATA.map((item, index) => {
         return item.childrens ? (
@@ -22,13 +27,19 @@ export const ServiceAccordion = () => {
             value={`item-${index}`}
             className="bg-brand-bg border border-transparent rounded-[12px] hover:border-brand transition-colors"
           >
-            <AccordionTrigger className="px-8 py-6 text-xl xl:text-2xl text-brand-foreground cursor-pointer">
-              <Link href={item.path}>{item.title}</Link>
-            </AccordionTrigger>
+            <Link href={item.path}>
+              <AccordionTrigger className="px-8 py-6 text-xl xl:text-2xl text-brand-foreground cursor-pointer">
+                {item.title}
+              </AccordionTrigger>
+            </Link>
             <AccordionContent className="px-8 py-6 text-xl xl:text-2xl text-brand-foreground">
               <div className="flex flex-col gap-4">
                 {item.childrens.map((childItem, childIndex) => (
-                  <Link key={childIndex} href={childItem.path} className="text-brand-foreground/50">
+                  <Link
+                    key={childIndex}
+                    href={childItem.path}
+                    className="text-brand-foreground/50"
+                  >
                     {childItem.title}
                   </Link>
                 ))}
@@ -36,12 +47,14 @@ export const ServiceAccordion = () => {
             </AccordionContent>
           </AccordionItem>
         ) : (
-          <div
+          <Link
             key={index}
-            className="bg-brand-bg px-8 py-6 border border-transparent rounded-[12px] hover:border-brand transition-colors cursor-pointer"
+            href={item.path}
+            onClick={() => setOpenItem(null)}
+            className="bg-brand-bg px-8 py-6 border border-transparent rounded-[12px] hover:border-brand cursor-pointer text-xl xl:text-2xl font-semibold text-brand-foreground transition-all"
           >
-            <Link href={item.path} className="text-xl xl:text-2xl font-semibold text-brand-foreground transition-all">{item.title}</Link>
-          </div>
+            {item.title}
+          </Link>
         );
       })}
     </Accordion>
